d2dd89243d5edfe98322be5d3733dde417f05f30
Zvki1 <rzaki@hotmail.fr>
2024-10-18 23:47:36 +0100

    feat:add edit client

---
+++ server/controllers/clients/editClientController.js
@@ -0,0 +1,29 @@
+const User = require("../../models/user");
+const bcrypt = require("bcryptjs");
+const editClientController = async (req, res) => {
+  const { _id } = req.params;
+  const { name, mail, password, max_bw } = req.body;
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    if (user.isAdmin) {
+      return res.status(403).json({ message: "You can't edit an admin" });
+    }
+    if (name !== undefined) user.name = name;
+    if (mail !== undefined) user.mail = mail;
+    if (password !== undefined) {
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+    }
+    if (max_bw !== undefined) user.max_bw = max_bw;
+
+    await user.save();
+    res.json({ user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = editClientController;
--- server/routers/clientsRoutes.js
+++ server/routers/clientsRoutes.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 const getClientsController = require("../controllers/clients/getClientsController");
 const getClientController = require("../controllers/clients/getClientController");
+const editClientController = require("../controllers/clients/editClientController");
+
 const isAdmin = require("../middlewares/isAdminMiddleware");
 
 
 router.get("/:_id", isAdmin, getClientController);
 router.get("/", isAdmin, getClientsController);
+router.patch("/:_id", isAdmin,editClientController);
 
 module.exports = router;
