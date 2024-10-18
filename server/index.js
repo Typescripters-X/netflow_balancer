f8eddfb11dd5edec62d8fdb2a4ec3e8e383a3e73
Abdelaziz-LAISSAOUI <azizaissa2004@gmail.com>
2024-10-18 20:18:25 +0100

    simulation

--- server/index.js
+++ server/index.js
@@ -1,60 +1,11 @@
-const express = require('express')
-const path = require('path');
-const app = express()
+import express from "express";
+import { default as simulationRouter } from "./simulation.js";
+const app = express();
 
-const port = 3000
+const port = 3000;
 
- 
-// const FILES_DIR = "files"
-
-const clients = []
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-
-app.get('/file', function (req, res) {
-
-    clients.push(req.ip)
-
-    const options = {
-        root: path.join(__dirname, "files")
-    };
-
-    const fileName = 'text.txt';
-    
-
-    res.sendFile(fileName, options, function (err) {
-        if (err) {
-            console.error('Error sending file:', err);
-        } else {
-            console.log('Sent:', fileName);
-        }
-    });
-
-
-
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use("/simulate", simulationRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- server/script.sh
+++ server/script.sh
@@ -1,6 +1,14 @@
- curl http://localhost:3000/file
- curl http://localhost:3000/file
- curl http://localhost:3000/file
- curl http://localhost:3000/file
- curl http://localhost:3000/file
- curl http://localhost:3000/file
+ curl --parallel --parallel-immediate --parallel-max 16 \
+http://localhost:3000/simulate\?username\=akram \
+http://localhost:3000/simulate\?username\=aziz \
+http://localhost:3000/simulate\?username\=zaki \
+http://localhost:3000/simulate\?username\=raouf \
+http://localhost:3000/simulate\?username\=a1 \
+http://localhost:3000/simulate\?username\=a2 \
+http://localhost:3000/simulate\?username\=a3 \
+http://localhost:3000/simulate\?username\=a4 \
+http://localhost:3000/simulate\?username\=a5 \
+http://localhost:3000/simulate\?username\=a6 \
+http://localhost:3000/simulate\?username\=a7 \
+http://localhost:3000/simulate\?username\=a \
+
