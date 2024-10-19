50aa84853e3c10c87286b98c72afb78d95d1257c
Abdelaziz-LAISSAOUI <azizaissa2004@gmail.com>
2024-10-19 22:42:39 +0100

    fix cors error

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 const cors = require("cors");
 
 const corsOptions = {
-  origin: ['http://localhost:5173'], 
+  origin: 'http://localhost:5173/', 
   methods: ['GET', 'POST', 'PUT', 'PATCH'], 
 };
 
