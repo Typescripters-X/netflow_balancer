57298f200105977db64a3f133cb78255298d191e
Abdelaziz-LAISSAOUI <azizaissa2004@gmail.com>
2024-10-19 23:56:35 +0100

    script fix

--- server/script.sh
+++ server/script.sh
@@ -1,14 +1,16 @@
+# this was used before adding middleware
+
  curl --parallel --parallel-immediate --parallel-max 16 \
 http://localhost:3000/simulate\?username\=akram \
 http://localhost:3000/simulate\?username\=aziz \
 http://localhost:3000/simulate\?username\=zaki \
 http://localhost:3000/simulate\?username\=raouf \
-#http://localhost:3000/simulate\?username\=ali \
-#http://localhost:3000/simulate\?username\=mohamed \
-#http://localhost:3000/simulate\?username\=abdesslam \
-#http://localhost:3000/simulate\?username\=rafik \
-#http://localhost:3000/simulate\?username\=ahmed \
-#http://localhost:3000/simulate\?username\=oussama \
-#http://localhost:3000/simulate\?username\=abdelkader \
-#http://localhost:3000/simulate\?username\=riad \
+http://localhost:3000/simulate\?username\=ali \
+http://localhost:3000/simulate\?username\=mohamed \
+http://localhost:3000/simulate\?username\=abdesslam \
+http://localhost:3000/simulate\?username\=rafik \
+http://localhost:3000/simulate\?username\=ahmed \
+http://localhost:3000/simulate\?username\=oussama \
+http://localhost:3000/simulate\?username\=abdelkader \
+http://localhost:3000/simulate\?username\=riad \
 
