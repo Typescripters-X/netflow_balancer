dade4de200cbdf3a62d3a1f20821b7bff9d86630
akrambj01 <bedjaoui.akram@intellisolutions-dz.com>
2024-10-19 08:46:12 +0100

    basic routing system

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,28 @@
+import React, { Suspense } from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+
+
+const LoginPage = React.lazy(() => import("./pages/auth/LoginPage"));
+const OverViewPage = React.lazy(() => import("./pages/admin/OverViewPage"));
+const ClientsPage = React.lazy(() => import("./pages/admin/ClientsPage"));
+const NotFoundPage = React.lazy(() => import("./pages/NotFoundPage"));
+
 
 const App = () => {
   return (
-    <div className="text-center">App</div>
+    <Suspense fallback={<h2>loading... </h2>}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/dashboard" element={<ProtectedRoute />}>
+            <Route index element={<OverViewPage />} />
+            <Route path="clients" element={<ClientsPage />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   )
 }
 
